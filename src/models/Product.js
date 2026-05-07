4e7a4390187fe5d56abc293684e77ff91ee1c034
codedcollections <ma.unander@gmail.com>
2026-05-07 11:03:02 +0200

    issue 2 add min price limit to product model to pass negative price test

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,24 +1,25 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
 const productSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, 'Product name is required'],
+      required: [true, "Product name is required"],
       trim: true,
     },
     price: {
       type: Number,
-      required: [true, 'Product price is required'],
+      min: 0,
+      required: [true, "Product price is required"],
     },
     description: {
       type: String,
-      default: '',
+      default: "",
     },
   },
   {
     timestamps: true,
-  }
-);
+  },
+)
 
-module.exports = mongoose.model('Product', productSchema);
+module.exports = mongoose.model("Product", productSchema)
